2e516150941928dfa4df07ad5cea6bd38a33ffa4
Dr. Wevelin Matos <drmatos.cardio@gmail.com>
2026-04-17 13:56:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 import { initMainTabs } from './router.js';
+import { renderNeonato } from './tabs/neonato/index.js';
 
 function bootstrap() {
   initMainTabs();
-
-  document.getElementById('tab-neo').innerHTML = '<h2>Neonato OK</h2><p>Render manual funcionando.</p>';
-  document.getElementById('tab-cresc').innerHTML = '<h2>Crescimento OK</h2><p>Render manual funcionando.</p>';
-  document.getElementById('tab-cardio').innerHTML = '<h2>Cardio OK</h2><p>Render manual funcionando.</p>';
+  renderNeonato();
 }
 
 document.addEventListener('DOMContentLoaded', bootstrap);
